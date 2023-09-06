46c8aa9595c2dabb7c1b51791a595ee39b88bbc1
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:13:14 +0530

    Add project files.

---
+++ Assignment 3(4)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyBaseClass
+{
+    protected int protectedField = 99;
+
+    protected void ProtectedMethod()
+    {
+        Console.WriteLine("This is a protected method.");
+    }
+}
+
+class MyDerivedClass : MyBaseClass
+{
+    public void AccessProtectedMembers()
+    {
+        Console.WriteLine("Accessing protected field from the derived class: " + protectedField);
+        ProtectedMethod();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDerivedClass derivedObject = new MyDerivedClass();
+
+        derivedObject.AccessProtectedMembers();
+    }
+}
